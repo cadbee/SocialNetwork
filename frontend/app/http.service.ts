83b4fc15a5b0d4c0760f13794edd4192686abdbe
cadbee <aschiam.net@gmail.com>
2023-02-27 11:18:05 +0300

    Frontend Render 1.0

--- frontend/app/http.service.ts
+++ frontend/app/http.service.ts
@@ -2,50 +2,50 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 const PORT = 3333;
-
+const URL = 'https://social-network-1oi4.onrender.com';
 @Injectable()
 export class HttpService {
   constructor(private http: HttpClient) {
   }
 
   addFriend(id, body): any {
-    return this.http.post('http://localhost:' + PORT + '/site/' + id + '/addfriends', body);
+    return this.http.post(URL + '/site/' + id + '/addfriends', body);
   }
 
   delFriend(id, body): any {
-    return this.http.post('http://localhost:' + PORT + '/site/' + id + '/delfriends', body);
+    return this.http.post(URL + '/site/' + id + '/delfriends', body);
   }
 
   addUser(body): any {
-    return this.http.put('http://localhost:' + PORT + '/site', body);
+    return this.http.put(URL + '/site', body);
   }
 
   changeFIO(id, body): any {
-    return this.http.post('http://localhost:' + PORT + '/site/' + id, body);
+    return this.http.post(URL + '/site/' + id, body);
   }
 
   getUsers(): any {
-    return this.http.get('http://localhost:' + PORT + '/site');
+    return this.http.get(URL + '/site');
   }
 
   getPass(): any {
-    return this.http.get('http://localhost:' + PORT + '/login');
+    return this.http.get(URL + '/login');
   }
 
   getUser(id): any {
-    return this.http.get('http://localhost:' + PORT + '/site/' + id);
+    return this.http.get(URL + '/site/' + id);
   }
 
   getUserinf(id): any {
-    return this.http.get('http://localhost:' + PORT + '/site/' + id + '/inf');
+    return this.http.get(URL + '/site/' + id + '/inf');
   }
 
   getUsersinf(id): any {
-    return this.http.get('http://localhost:' + PORT + '/site/' + id + '/friendsnews');
+    return this.http.get(URL + '/site/' + id + '/friendsnews');
   }
 
   setupLabels(): any {
-    return this.http.get('http://localhost:' + PORT + '/');
+    return this.http.get(URL + '/');
   }
 
 }
